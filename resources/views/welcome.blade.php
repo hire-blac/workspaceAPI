a27dda9b7e17e55fe8a75c7e3350b9212896e71c
danihire@gmail.com <danihire@gmail.com>
2022-05-24 15:15:06 +0100

    bootstrap cdn

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,10 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <!-- Styles -->
+        {{-- CDN --}}
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+        {{-- Local --}}
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
         <title>Laravel</title>
@@ -156,5 +160,8 @@
         </div>
     </body>
       <!-- Scripts -->
+      {{-- CDN --}}
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+      {{-- Local --}}
       <script src="{{ asset('js/app.js') }}" defer></script>
 </html>
