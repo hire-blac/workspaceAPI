cfe9416a433bf6e6d917ab384658ce2fa666ab87
danihire@gmail.com <danihire@gmail.com>
2022-05-25 14:18:43 +0100

    cors middleware

--- routes/api.php
+++ routes/api.php
@@ -21,20 +21,22 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::group(['prefix'=>'api', 'middleware'=>'cors']);
+Route::group(['prefix'=>'api', 'middleware'=>'cors'], function(){
 
-Route::get("space-type/{id?}", [SpaceTypeController::class, "index"]);
-Route::post("space-type/new", [SpaceTypeController::class, "store"]);
-Route::put("space-type/{id}/update", [SpaceTypeController::class, "update"]);
-Route::delete("space-type/{id}/delete", [SpaceTypeController::class, "destroy"]);
-
-Route::get("space/{id?}", [SpaceController::class, "index"]);
-Route::post("space/new", [SpaceController::class, "store"]);
-Route::put("space/{id}/update", [SpaceController::class, "update"]);
-Route::delete("space/{id}/delete", [SpaceController::class, "destroy"]);
-
-Route::get("bookings/{id?}", [BookingsController::class, "index"]);
-Route::get("bookings/day-bookings/{day}", [BookingsController::class, "day_bookings"]);
-Route::get("bookings/day-bookings/{day}/available", [BookingsController::class, "available_space"]);
-Route::post("bookings/new", [BookingsController::class, "store"]);
-Route::delete("bookings/{id}/delete", [BookingsController::class, "destroy"]);
+  Route::get("space-type/{id?}", [SpaceTypeController::class, "index"]);
+  Route::post("space-type/new", [SpaceTypeController::class, "store"]);
+  Route::put("space-type/{id}/update", [SpaceTypeController::class, "update"]);
+  Route::delete("space-type/{id}/delete", [SpaceTypeController::class, "destroy"]);
+  
+  Route::get("space/{id?}", [SpaceController::class, "index"]);
+  Route::post("space/new", [SpaceController::class, "store"]);
+  Route::put("space/{id}/update", [SpaceController::class, "update"]);
+  Route::delete("space/{id}/delete", [SpaceController::class, "destroy"]);
+  
+  Route::get("bookings/{id?}", [BookingsController::class, "index"]);
+  Route::get("bookings/day-bookings/{day}", [BookingsController::class, "day_bookings"]);
+  Route::get("bookings/day-bookings/{day}/available", [BookingsController::class, "available_space"]);
+  Route::post("bookings/new", [BookingsController::class, "store"]);
+  Route::delete("bookings/{id}/delete", [BookingsController::class, "destroy"]);
+  
+});
