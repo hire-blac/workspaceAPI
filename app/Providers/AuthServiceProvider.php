ee4e272ca10f4b653eefb9c578583e28375e6f52
danihire@gmail.com <danihire@gmail.com>
2022-07-07 08:40:07 +0100

    passport oauth keys

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -36,5 +36,7 @@ class AuthServiceProvider extends ServiceProvider
         Passport::setDefaultScope([
             'customer',
         ]);
+
+        Passport::loadKeysFrom('/secret-keys/oauth');
     }
 }
